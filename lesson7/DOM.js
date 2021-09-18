d3463d738b50e4812eba00512a50290340bb35fe
vinhdq2005 <vinhdq2005@gmail.com>
2021-09-18 21:39:23 +0700

    bài bóng đèn

---
+++ lesson7/DOM.js
@@ -0,0 +1,19 @@
+// let a = document.getElementById("heading")
+// console.log(a);
+
+// a.style.color = "yellow"
+// a.style.fontsize = "100px"
+// console.dir(a)
+
+
+let a = document.getElementById("anh")
+
+console.dir(a)
+
+function turnOn() {
+    a.src = "https://www.w3schools.com/js/pic_bulbon.gif?fbclid=IwAR2s7Px1OOo07mR6VXJf4ngAQ-ewxsQCGiXEyAA_7_maxyBVU7b0iSXzTXk"
+}
+
+function turnOff() {
+    a.src = "https://www.w3schools.com/js/pic_bulboff.gif?fbclid=IwAR2in7dr_Iqyb6jA1mSVQE7obMm4h00eBkyrarZwHAEwJQGLDd_Gfe4pZSI"
+}
