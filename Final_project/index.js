fea94e90d7d76fb1bc63602b338b884840de99d0
vinhdq2005 <vinhdq2005@gmail.com>
2021-09-23 17:07:56 +0700

    D&F

--- Final_project/index.js
+++ Final_project/index.js
@@ -10,6 +10,18 @@ function ul(index) {
     }
 }
 
+// search bar
+
+let searchBar = document.querySelector(".search-bar")
+let searchIcon = document.querySelector(".search-icon")
+
+function openSearchbar() {
+    searchBar.classList.add("open-search-bar")
+    searchIcon.classList.add("close-search-icon")
+}
+
+searchIcon.addEventListener('click', openSearchbar)
+
 // slide
 let imangeSlide = [
     {url: "https://static.wixstatic.com/media/11062b_91a611d9fc74418da54bb15a2e85481d~mv2.jpg/v1/fill/w_1349,h_900,al_c,q_85,usm_0.66_1.00_0.01/11062b_91a611d9fc74418da54bb15a2e85481d~mv2.jpg"},
@@ -19,23 +31,37 @@ let imangeSlide = [
     {url: ""}
 ]
 
+// slide
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider' ).mount();
+} );
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider', {
+		'cover'      : true,
+		'heightRatio': 0.5,
+	} ).mount();
+} );
 
-let slider = document.querySelector("#slider") 
-let changeSlidebtns = document.querySelectorAll(".button-changeslide")
 
+// login page
 
-function colorBtn() {
-    changeSlidebtns.style.background = "white"
+let login = document.querySelector(".account")
+let loginPage = document.querySelector("#login")
+let loginContainer = document.querySelector(".login-container")
+
+function showLogin() {
+    loginPage.classList.add('open')
 }
 
-function changeSlide() {
-    slider.style.backgroundImage = url(imageSlide[0].url)
-}
-
-for (const changeSlidebtn of changeSlidebtns) {
-    changeSlidebtn.addEventListener('click', changeSlide)
+function hideLogin() {
+    loginPage.classList.remove('open')
 }
 
 
+loginPage.addEventListener('click', hideLogin)
+login.addEventListener('click', showLogin)
 
+loginContainer.addEventListener('click', function(event) {
+    event.stopPropagation()
+})
 
