687dcf16fb34007ffc91409353668dca1735e590
vinhdq2005 <vinhdq2005@gmail.com>
2021-09-25 21:54:30 +0700

    D&F

--- Final_project/js/index.js
+++ Final_project/js/index.js
@@ -5,10 +5,13 @@ function ul(index) {
     
     var underlines = document.querySelectorAll(".underline");
 
+
     for (var i = 0; i < underlines.length; i++) {
         underlines[i].style.transform = 'translate3d(' + index * 100 + '%,0,0)';
     }
 }
+
+
 
 // search bar
 
