fffc3b60fc4409f9a557fbb16c1367944b61ea6d
vinhdq2005 <vinhdq2005@gmail.com>
2022-06-29 23:09:24 +0700

    Chat

--- JSI06/lesson2/chat/index.js
+++ JSI06/lesson2/chat/index.js
@@ -2,6 +2,9 @@ document.querySelector("#sw-darkmode").addEventListener("click", () => {
   document.body.classList.toggle("darkmode");
 });
 
+var email_ = ""
+var currentid_ = ""
+var img_ = ""
 
 // check xem đã đăng nhập hay chưa
 firebase.auth().onAuthStateChanged((user) => {
@@ -10,36 +13,76 @@ firebase.auth().onAuthStateChanged((user) => {
     // https://firebase.google.com/docs/reference/js/firebase.User
     var uid = user.uid;
     console.log(user.email);
-    loadchat(user.email)
+    email_ = user.email
+    img_ = user.photoURL
+    loadchat(user.email);
+    console.log(user);
+    renderCurrentUser(user.photoURL, user.displayName);
     // ...
   } else {
     // User is signed out
-    sweetAlert("warning", "bạn cần đăng nhập")
-    setTimeout(()=>{
-      open("../signin/index.html","_self")
-    }, 3000)
+    sweetAlert("warning", "bạn cần đăng nhập");
+    setTimeout(() => {
+      open("../signin/index.html", "_self");
+    }, 3000);
   }
 });
 
 let signOut = () => {
-  firebase.auth().signOut().then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-}
-
-
-let loadchat = async (email) => {
-  let result = await firebase.firestore().collection("chat").where("users", "array-contains", email).get();
-  console.log(result);
-  let data = getDataFromDocs(result.docs)
-  console.log(data);
-  
-  renderChat(data[0], email)
-  renderUserList(data)
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 };
 
+let clock = () => {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let d = date.getDay();
+
+  if (m < 10) {
+    m = "0" + m;
+  }
+  if (h < 10) {
+    h = "0" + h;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  }
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d];
+
+  return ` ${h}:${m}:${s}     ${n}`;
+};
+
+let loadchat = async (email) => {
+  let result = await firebase
+    .firestore()
+    .collection("chat")
+    .where("users", "array-contains", email)
+    .get();
+  console.log(result);
+  let data = getDataFromDocs(result.docs);
+  console.log(data);
+
+  renderChat(data[0], email);
+  renderUserList(data, email);
+};
 
 let getDataFromDoc = (doc) => {
   let data = doc.data();
@@ -56,53 +99,55 @@ let getDataFromDocs = (docs) => {
   return result;
 };
 
-
 let sweetAlert = (icon, message) => {
   const Toast = Swal.mixin({
     toast: true,
-    position: 'top-end',
+    position: "top-end",
     showConfirmButton: false,
     timer: 3000,
     timerProgressBar: true,
     didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
-  
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
   Toast.fire({
     icon: icon,
-    title: message
-  })
-}
-
-
+    title: message,
+  });
+};
 
 let renderChat = (data, ownerEmail) => {
-  let chat = data.chat
-  let dom = document.querySelector(".chat-panel")
-  dom.innerHTML = ""
+  let chat = data.chat;
+  currentid_ = data.id
+  let dom = document.querySelector(".chat-panel");
+  dom.innerHTML = "";
 
   for (let i = 0; i < chat.length; i++) {
     let html = `<div class="no-gutters">
     <div class="col-md-5 ${chat[i].email == ownerEmail ? "offset-md-7" : ""}">
-      <div class="chat-bubble ${chat[i].email == ownerEmail ? "chat-bubble--blue chat-bubble--right" : "chat-bubble--left"} ">
+      <div class="chat-bubble ${
+        chat[i].email == ownerEmail
+          ? "chat-bubble--blue chat-bubble--right"
+          : "chat-bubble--left"
+      } ">
       <p>${chat[i].email}</p>  
       ${chat[i].content}
       </div>
     </div>
-  </div>`
+  </div>`;
 
-  dom.innerHTML += html
+    dom.innerHTML += html;
   }
-}
+};
 
-let renderUserList = (data) => {
-  let dom = document.querySelector(".list-friend")
-  dom.innerHTML = ""
-  
+let renderUserList = (data, email) => {
+  let dom = document.querySelector(".list-friend");
+  dom.innerHTML = "";
+
   for (let i = 0; i < data.length; i++) {
-    let html = `<div class="friend-drawer friend-drawer--onhover">
+    let html = `<div id="c${data[i].id}" class="friend-drawer friend-drawer--onhover">
     <img src="${data[i].avatar}" alt="" class="profile-image">
     <div class="text">
       <h6>${data[i].name}</h6>
@@ -111,10 +156,52 @@ let renderUserList = (data) => {
       ${data[i].timeStart}
     </span>
   </div>
-  <hr>`
+  <hr>`;
 
-  dom.innerHTML += html
+    dom.innerHTML += html;
   }
-}
 
+  for (let i = 0; i < data.length; i++) {
+    let user = document.querySelector(`#c${data[i].id}`);
+    user.addEventListener("click", () => {
+      renderChat(data[i], email);
+    });
+  }
+};
 
+let renderCurrentUser = (photo, userName) => {
+  document.querySelector("#userImg").src = photo;
+  document.querySelector("#userName").innerHTML = userName;
+};
+
+setInterval(() => {
+  document.querySelector("#realTime").innerHTML = clock();
+}, 1000);
+
+let formChat = document.querySelector("#chat_input_form");
+formChat.onsubmit = (e) => {
+  e.preventDefault();
+
+  let content = formChat.chat.value.trim();
+
+  updateNewMessage(content, email_, img_, clock(), currentid_)
+};
+
+let updateNewMessage = async (content, email, img, time, currentID) => {
+  if (currentID) {
+    let conversationId = currentID;
+    let message = {
+      content: content,
+      time: time,
+      img: img,
+      email: email,
+    };
+    await firebase
+      .firestore()
+      .collection("chat")
+      .doc(conversationId)
+      .update({
+        chat: firebase.firestore.FieldValue.arrayUnion(message),
+      });
+  }
+};
