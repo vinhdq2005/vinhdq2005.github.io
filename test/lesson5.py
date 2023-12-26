7e20f86b23047a3d5a752731d479d5e5f1662dd4
vinhdq2005 <vinhdq2005@gmail.com>
2023-12-26 10:15:48 +0700

    lesson5

--- test/lesson5.py
+++ test/lesson5.py
@@ -1,6 +1,14 @@
 n = 5
 # vòng lặp while
+print("Hình tam giác")
 while n>0:
     print("*"*n)
     n -= 1
-    
+# ve hình chóp ngượcZZ
+print("Hình chóp ngược")
+k = 7
+i = 0
+while k>0:
+    print(" "*i, "*"*k)
+    i += 1
+    k -= 2
